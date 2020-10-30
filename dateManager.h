d7b217ab657b1f2776b031a0d00882b18afcceb4
josephBlake <blakeaj@msoe.edu>
2020-10-30 13:52:00 -0400

    Removed futureDate files.

--- dateManager.h
+++ dateManager.h
@@ -6,12 +6,16 @@ using namespace std;
 
 class DateManager {
 public:
+  // This is the constructor which will store a date for method usage.
   DateManager(string date);
 
+  // The purpose of this method is to check what the end day of a month is based a date's current month.
   int checkMonth(int month, int year);
 
+  // This method is the main function for the -f command in the program. This method returns what date it will be after a certain amount of days after a given date.
   string calculateFutureDate(int daysPast);
 
+  // This method is the main function for the -w command in the program. This method returns what day of the week a given date is in.
   string dayOfTheWeek();
 
   ~DateManager();
